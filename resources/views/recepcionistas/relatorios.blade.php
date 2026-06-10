6898c74433bd793a9e5c8d0e75b3a2633e62b0cb
peal-26 <edilasio@live.com>
2026-06-10 20:32:39 +0100

    fix: Api Relatório Consultas Recepcionista

--- resources/views/recepcionistas/relatorios.blade.php
+++ resources/views/recepcionistas/relatorios.blade.php
@@ -237,6 +237,7 @@
         const csrfToken = "{{ csrf_token() }}";
         const gerar_relatorio_consultas_btn = document.getElementById('gerar_relatorio_consultas_btn')
         gerar_relatorio_consultas_btn.addEventListener('click', async (e) => {
+            e.preventDefault()
             const id_paciente = document.getElementById('id_paciente').value || null
             const estado = document.getElementById('estado').value || null
             const id_recepcionista = document.getElementById('id_recepcionista').value || null
@@ -245,7 +246,6 @@
             const data_inicio = document.getElementById('data_inicio').value || null
             const data_fim = document.getElementById('data_fim').value || null
             try {
-                e.preventDefault()
                 const resultado = await fetch(url, {
                     method: "POST",
                     headers: {
@@ -279,48 +279,6 @@
                 console.error("Erro ao gerar relatório de consultas :", error);
                 mostrarMensagemErro(
                     "Ocorreu um erro ao gerar relatório de consultas. Por favor, tente novamente.\n" +
-                    error?.message || "",
-                );
-            }
-        })
-        const gerar_relatorio_prontuario_btn = document.getElementById('gerar_relatorio_prontuario_btn')
-        gerar_relatorio_prontuario_btn.addEventListener('click', async (e) => {
-            const id_paciente = document.getElementById('id_paciente_prontuario')
-            const data_inicio = document.getElementById('data_inicio_prontuario')
-            const data_fim = document.getElementById('data_fim_prontuario')
-            const rota =
-                "{{ route('api_relatorio_prontuario_paciente', ['id_paciente' => ':id', 'data_inicio' => 'data_inicio_param', 'data_fim' => 'data_fim_param']) }}"
-            const url = rota.replace(':id', id_paciente.value)
-                .replaceAll('data_inicio_param', data_inicio.value ? encodeURIComponent(data_inicio.value) : '')
-                .replaceAll('data_fim_param', data_fim.value ? encodeURIComponent(data_fim.value) : '')
-                .replaceAll('amp;', '')
-            try {
-                e.preventDefault()
-                const resultado = await fetch(url, {
-                    method: "GET",
-                    headers: {
-                        "Content-Type": "application/json",
-                        "X-CSRF-TOKEN": csrfToken,
-                    },
-
-                })
-                const dados = await resultado.json();
-                if (!resultado.ok) {
-                    throw new Error(
-                        dados?.erro || "Erro ao gerar relatório de prontuario",
-                    );
-                }
-                const descricao = `${dados.nome} `
-
-                const logotipo = await obterImagemBase64(logo_url)
-                gerarRelatorioProntuarioPacienteTabela(dados, {
-                    logotipo,
-                    descricao
-                })
-            } catch (error) {
-                console.error("Erro ao gerar relatório de prontuario :", error);
-                mostrarMensagemErro(
-                    "Ocorreu um erro ao gerar relatório de prontuario. Por favor, tente novamente.\n" +
                     error?.message || "",
                 );
             }
